51016b57a61d17376dffbdfd5437daec4a36b161
Atik2788 <ashanto653.2@gmail.com>
2025-12-13 22:30:35 +0600

    first commit

---
+++ src/layout/CommonLayout.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+
+interface IProps{
+    children: ReactNode
+}
+
+const CommonLayout = ({children}: IProps) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default CommonLayout;
